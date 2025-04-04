d6cdc432c179d8fc2210a242ef2e45405c35f9f4
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2025-04-04 18:10:52 +0530

    fist commit

---
+++ src/displayName.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function DisplayName() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && lastName) {
+      setFullName(`${firstName} ${lastName}`);
+    }
+  };
+
+  return (
+    <div >
+      <form onSubmit={handleSubmit}>
+      <div><label>First Name</label>
+        <input
+          type="text"
+          placeholder="Enter first name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        /></div>  
+        <div><label>Last Name</label>
+        <input
+          type="text"
+          placeholder="Enter last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        /></div>
+        <button type="submit" disabled={!firstName || !lastName}>
+          Submit
+        </button>
+      </form>
+      {fullName && <p>Full Name: {fullName}</p>}
+    </div>
+  );
+}
